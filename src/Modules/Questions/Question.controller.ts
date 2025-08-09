bc46bc47bcd8b833c07ed580b50baf6bf60f13ba
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-08-09 13:13:24 +0600

    question mdoule done

---
+++ src/Modules/Questions/Question.controller.ts
@@ -0,0 +1,84 @@
+import { RequestHandler } from "express";
+import { emptyResponse, notUpdated, success } from "../../Utils/response";
+import { questionSchema } from "./Question.validation";
+import { questionServices } from "./Question.service";
+
+// Create a new question
+const createQuestion: RequestHandler = async (req, res, next) => {
+  try {
+    const validatedData = questionSchema.parse(req.body);
+    const result = await questionServices.createquestion(validatedData);
+    success(res, result, "question created");
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+// Get all questions (with optional isActive filter)
+const getQuestion: RequestHandler = async (req, res, next) => {
+  try {
+    const query = req.query;
+    const questions = await questionServices.getquestions(query);
+    if (questions.length === 0) {
+      emptyResponse(res, questions);
+      return;
+    }
+    success(res, questions, "questions retrieved", questions.length);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get a single question by ID
+const getSingleQuestion: RequestHandler = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const question = await questionServices.getquestionById(id);
+    if (!question) {
+      notUpdated(res, id, null);
+      return;
+    }
+    success(res, question, "question retrieved");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update a question
+const updateQuestion: RequestHandler = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const validatedData = questionSchema.partial().parse(req.body); // allow partial updates
+    const updatedquestion = await questionServices.updatequestion(id, validatedData);
+    if (!updatedquestion) {
+      emptyResponse(res, null);
+      return;
+    }
+    success(res, updatedquestion, "question updated");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Soft delete a question
+const deleteQuestion: RequestHandler = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const deletedquestion = await questionServices.deletequestion(id);
+    if (!deletedquestion) {
+      notUpdated(res, id, null);
+      return;
+    }
+    success(res, deletedquestion, "question deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const questionController = {
+  createQuestion,
+  getQuestion,
+  getSingleQuestion,
+  updateQuestion,
+  deleteQuestion,
+};
---
+++ src/Modules/Questions/Question.service.ts
@@ -0,0 +1,39 @@
+import { TQuestion } from "./Question.interface";
+import { QuestionModel } from "./Question.model";
+
+const createquestion = async (data: TQuestion) => {
+  return await QuestionModel.create(data);
+};
+
+const getquestions = async (query: any) => {
+  const queryData: Record<string, any> = {
+    isDeleted: false,
+  };
+  return await QuestionModel.find(queryData).select("--v").sort({ createdAt: -1 });
+};
+
+// Get a single question by ID
+const getquestionById = async (id: string) => {
+  return await QuestionModel.findOne({ _id: id, isDeleted: false }).select("-__v");
+};
+
+// Update a question
+const updatequestion = async (id: string, data: Partial<TQuestion>) => {
+  return await QuestionModel.findByIdAndUpdate(id, data, {
+    new: true,
+    runValidators: true,
+  });
+};
+
+// Soft-delete a question
+const deletequestion = async (id: string) => {
+  return await QuestionModel.findByIdAndUpdate(id, { isDeleted: true }, { new: true });
+};
+
+export const questionServices = {
+  createquestion,
+  getquestions,
+  getquestionById,
+  updatequestion,
+  deletequestion,
+};
