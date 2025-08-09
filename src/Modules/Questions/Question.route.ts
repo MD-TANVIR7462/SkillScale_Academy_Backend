bcd33bc759c8491286ee0a99ebf50f313f344033
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-08-09 13:14:30 +0600

    question route added

--- src/Modules/Questions/Question.route.ts
+++ src/Modules/Questions/Question.route.ts
@@ -1,1 +1,14 @@
 
+import { Router } from "express";
+import { questionController } from "./Question.controller";
+
+const router = Router();
+
+router.post("/create-question", questionController.createQuestion);
+router.get("/get-question", questionController.getQuestion);
+router.get("/get-question/:id", questionController.getSingleQuestion);
+router.patch("/update-question/:id", questionController.updateQuestion);
+router.delete("/delete-question/:id", questionController.deleteQuestion);
+
+
+export const customerRouter = router;
